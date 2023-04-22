59c4650b2d356190b0db8e1fcdd31b7c000b1ab9
Mikeyvon <yvonlcy@gmail.com>
2023-04-23 04:14:49 +0800

    verify the updates

--- index.js
+++ index.js
@@ -42,3 +42,8 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
